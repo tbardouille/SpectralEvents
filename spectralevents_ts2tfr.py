0755de215b0b5c7aad115197bfcf8eb797d9f972
Tim Bardouille <tim.bardouille@dal.ca>
2019-02-13 18:54:21 -0400

    ADDED: Content to find and characterize spectral events based on the 1st approach to finding local maxima

--- spectralevents_ts2tfr.py
+++ spectralevents_ts2tfr.py
@@ -22,28 +22,27 @@ import numpy as np
 #   -----------------------------------------------------------------------
 
 def spectralevents_ts2tfr (S,fVec,Fs,width):
-
-# spectralevents_ts2tfr(S,fVec,Fs,width);
-#
-# Calculates the TFR (in spectral power) of a time-series waveform by 
-# convolving in the time-domain with a Morlet wavelet.                            
-#
-# Input
-# -----
-# S    : signals = time x Trials      
-# fVec    : frequencies over which to calculate TF energy        
-# Fs   : sampling frequency
-# width: number of cycles in wavelet (> 5 advisable)  
-#
-# Output
-# ------
-# t    : time
-# f    : frequency
-# B    : phase-locking factor = frequency x time
-#     
-# Adapted from Ole Jensen's traces2TFR in the 4Dtools toolbox.
-#
-# See also SPECTRALEVENTS, SPECTRALEVENTS_FIND, SPECTRALEVENTS_VIS.
+	# spectralevents_ts2tfr(S,fVec,Fs,width);
+	#
+	# Calculates the TFR (in spectral power) of a time-series waveform by 
+	# convolving in the time-domain with a Morlet wavelet.                            
+	#
+	# Input
+	# -----
+	# S    : signals = time x Trials      
+	# fVec    : frequencies over which to calculate TF energy        
+	# Fs   : sampling frequency
+	# width: number of cycles in wavelet (> 5 advisable)  
+	#
+	# Output
+	# ------
+	# t    : time
+	# f    : frequency
+	# B    : phase-locking factor = frequency x time
+	#     
+	# Adapted from Ole Jensen's traces2TFR in the 4Dtools toolbox.
+	#
+	# See also SPECTRALEVENTS, SPECTRALEVENTS_FIND, SPECTRALEVENTS_VIS.
 
 	S = S.T
 	numTrials = S.shape[0]
@@ -64,18 +63,18 @@ def spectralevents_ts2tfr (S,fVec,Fs,width):
 	return TFR, tVec, fVec
 
 def energyvec(f,s,Fs,width):
-# Return a vector containing the energy as a
-# function of time for frequency f. The energy
-# is calculated using Morlet's wavelets. 
-# s : signal
-# Fs: sampling frequency
-# width : width of Morlet wavelet (>= 5 suggested).
+	# Return a vector containing the energy as a
+	# function of time for frequency f. The energy
+	# is calculated using Morlet's wavelets. 
+	# s : signal
+	# Fs: sampling frequency
+	# width : width of Morlet wavelet (>= 5 suggested).
 	
 	dt = 1/Fs
 	sf = f/width
 	st = 1/(2 * np.pi * sf)
 
-	t=-3.5*st:dt:3.5*st; #??? NOT SURE WHAT THIS DOES
+	t= np.arange(-3.5*st, 3.5*st, dt)
 	m = morlet(f, t, width)
 
 	y = np.convolve(s, m)
@@ -85,12 +84,12 @@ def energyvec(f,s,Fs,width):
 	return y
 
 def morlet(f,t,width):
-# Morlet's wavelet for frequency f and time t. 
-# The wavelet will be normalized so the total energy is 1.
-# width defines the ``width'' of the wavelet. 
-# A value >= 5 is suggested.
-#
-# Ref: Tallon-Baudry et al., J. Neurosci. 15, 722-734 (1997)
+	# Morlet's wavelet for frequency f and time t. 
+	# The wavelet will be normalized so the total energy is 1.
+	# width defines the ``width'' of the wavelet. 
+	# A value >= 5 is suggested.
+	#
+	# Ref: Tallon-Baudry et al., J. Neurosci. 15, 722-734 (1997)
 
 	sf = f/width
 	st = 1/(2 * np.pi * sf)
